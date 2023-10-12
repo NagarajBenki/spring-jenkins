065263fa30d9b9e14783fd76bbac16539b69c31b
nagar <nagar@LAPTOP-8MKSGNR5>
2023-10-12 08:04:40 +0530

    First Commit

---
+++ src/test/java/com/jenkins/api/SpringJenkinsApplicationTest.java
@@ -0,0 +1,14 @@
+package com.jenkins.api;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SpringJenkinsApplicationTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
